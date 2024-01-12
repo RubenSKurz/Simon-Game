f5066952f34b1947541ef19c5e240674102e5505
Diego Moura <diego.moura@theideaofnow.com>
2024-01-11 21:28:01 -0300

    refactoring

--- game.js
+++ game.js
@@ -1,88 +1,109 @@
-var gamePattern = [];
+// var gamePattern = [];
 
-var userClickedPattern = [];
+// var userClickedPattern = [];
 
-var buttonColors = ["red","blue","green","yellow"];
+// var buttonColors = ["red","blue","green","yellow"];
 
-var gameStarted = false;
+// var gameStarted = false;
 
-var level = 0;
+// var level = 0;
 
-$(document).on("keypress", function(){
-    if (gameStarted == false){
+let gamePattern = [];
+let userClickedPattern = [];
+let buttonColors = ["red", "blue", "green", "yellow"];
+let gameStarted = false;
+let level = 0;
+
+$(document).on("keypress", function () {
+    if (gameStarted == false) {
         nextSequence();
         gameStarted = true;
-    };
+    }
 });
 
-$(".btn").on("click", function(){ 
-    var userChosenColor = $(this).attr("id");
-    makeSound(userChosenColor);
-    animatePress(userChosenColor);
+$(".btn").on("click", function () {
+    let userChosenColor = $(this).attr("id");
     userClickedPattern.push(userChosenColor);
-    checkAnswer(userClickedPattern.length -1);
-});
+    playColor(userChosenColor);
+    checkAnswer();
+})
 
-function nextSequence() {
-    var randomnumber = Math.floor((Math.random() * 4));
-    var randomChosenColor = buttonColors [randomnumber];
-    $("#"+ randomChosenColor).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-    makeSound(randomChosenColor);
-    gamePattern.push(randomChosenColor);
-    level ++;
-    $("h1").text("Level " + level);
+function playColor(key) {
+    makeSound(key);
+    animatePress(key);
 }
 
-function makeSound(key){
-    var sound = new Audio("./sounds/" + key + ".mp3");   
+function makeSound(key) {
+    var sound = new Audio("./sounds/" + key + ".mp3");
     return sound.play();
 }
 
 function animatePress(key) {
-    $("#" + key).addClass("pressed");
-    setTimeout(function(){
-        $("#" + key).removeClass("pressed");
+    $("#" + key).toggleClass("pressed");
+    setTimeout(function () {
+        $("#" + key).toggleClass("pressed");
     }, 100);
 }
 
-function checkAnswer(currentLevel){
-    if (userClickedPattern[currentLevel] === gamePattern[currentLevel]){
-        if (userClickedPattern.length === gamePattern.length){   
-            setTimeout(function(){
-                oldPattern();
-            },1000)
-            setTimeout(function(){
-                nextSequence();}, 1000*gamePattern.length + 1000);
-            userClickedPattern = [];
-        }
-    }
-    else{
-        var gameOverSound = new Audio("./sounds/wrong.mp3");
-        gameOverSound.play();
-        $("body").addClass("game-over");
-        setTimeout(function(){
-            $("body").removeClass("game-over");
-        }, 200);
-        $("h1").text("GAME OVER, Press any key to restart");
-        startOver();
-    }
+function nextSequence() {
+    let randomNumber = Math.floor(Math.random() * 4);
+    let randomChosenColor = buttonColors[randomNumber];
+
+    gamePattern.push(randomChosenColor);
+
+    gamePattern.forEach(function (color, index) {
+        setTimeout(function () {
+            $("#" + color).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+            makeSound(color);
+        }, index * 1000);
+    });
+
+    level++;
+    $("h1").text("Level " + level);
 }
 
-function startOver(){
+function checkAnswer() {
+
+    if (userClickedPattern[userClickedPattern.length - 1] !== gamePattern[userClickedPattern.length - 1]) {
+        gameover();
+        return;
+    }
+
+    if (userClickedPattern.length !== gamePattern.length) {
+        return;
+    }
+
+    if (userClickedPattern.toString() !== gamePattern.toString()) {
+        gameover();
+        return;
+    }
+
+    setTimeout(function () {
+        nextSequence();
+    }, 1000);
+
+    userClickedPattern = [];
+
+}
+
+function startOver() {
     gameStarted = false;
     gamePattern = [];
     userClickedPattern = [];
     level = 0;
 }
 
+function gameover() {
+    let gameOverSound = new Audio("./sounds/wrong.mp3");
+    gameOverSound.play();
 
+    $("body").addClass("game-over");
 
-function oldPattern() {
-    for (let item = 0; item < gamePattern.length; item++){
-        setTimeout(function(){
-            $("#" + gamePattern[item]).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-        makeSound(gamePattern[item]);
-        },item*1000 )
-        
-    }
+    setTimeout(function () {
+        $("body").removeClass("game-over");
+    }, 200);
+
+    $("h1").text("GAME OVER, Press any key to restart");
+
+    startOver();
 }
